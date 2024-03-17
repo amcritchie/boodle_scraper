8a8137e8bc5e2a558f73152550530047be0ade5c
Alex <alex@planomatic.com>
2024-03-17 13:43:41 -0600

    Games data model and postgres connection

---
+++ db/migrate/20240317191458_create_games.rb
@@ -0,0 +1,31 @@
+class CreateGames < ActiveRecord::Migration[7.0]
+  def change
+    create_table :games do |t|
+      t.string :source
+      t.integer :season
+      t.string :week
+      t.string :away_team
+      t.string :home_team
+      t.integer :away_total
+      t.integer :home_total
+      t.date :date
+      t.float :over_under
+      t.float :over_under_odds
+      t.float :away_spread
+      t.float :home_spread
+      t.integer :away_ot
+      t.integer :home_ot
+      t.integer :away_q4
+      t.integer :home_q4
+      t.integer :away_q3
+      t.integer :home_q3
+      t.integer :away_q2
+      t.integer :home_q2
+      t.integer :away_q1
+      t.integer :home_q1
+      t.float :odds
+
+      t.timestamps
+    end
+  end
+end
