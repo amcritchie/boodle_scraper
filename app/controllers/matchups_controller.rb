870d19d307174549f7375abe852af327b854b23c
Alex McRitchie <amcritchie@gmail.com>
2025-07-21 23:47:03 -0600

    Week 1 summary page

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -10,6 +10,24 @@ class MatchupsController < ApplicationController
     @matchups = Matchup.where(season: 2025, week_slug: 1).order(:team_slug)
   end
 
+  def summary
+    @matchups = Matchup.where(season: 2025, week_slug: 1).order(passing_offense_score: :desc)
+    
+    # Get all teams seasons data for component rankings
+    @teams_seasons = TeamsSeason.where(season_year: 2025).includes(:team).order(:team_slug)
+    
+    # Get component rankings for the tables
+    @play_caller_rankings = TeamsSeason.where(season_year: 2025).play_callers.by_play_caller_rank
+    @qb_passing_rankings = TeamsSeason.where(season_year: 2025).qbs.by_grades_offense
+    @receiving_rankings = TeamsSeason.where(season_year: 2025).receiver_rankings
+    @pass_block_rankings = TeamsSeason.where(season_year: 2025).pass_block_rankings
+    @pass_rush_rankings = TeamsSeason.where(season_year: 2025).pass_rush_rankings
+    @coverage_rankings = TeamsSeason.where(season_year: 2025).coverage_rankings
+    @rushing_rankings = TeamsSeason.where(season_year: 2025).rushing_rankings
+    @run_block_rankings = TeamsSeason.where(season_year: 2025).run_block_rankings
+    @run_defense_rankings = TeamsSeason.where(season_year: 2025).run_defense_rankings
+  end
+
   def teams_seasons
     @teams_seasons = TeamsSeason.where(season_year: 2025).includes(:team).order(:team_slug)
     # Ranking data for tables
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   get 'matchups/2025/week1', to: 'matchups#week1', as: 'matchups_2025_week1'
   get 'matchups/2025/week1/roster', to: 'matchups#roster', as: 'matchups_2025_week1_roster'
+  get 'matchups/2025/week1/summary', to: 'matchups#summary', as: 'matchups_2025_week1_summary'
   get 'teams/seasons', to: 'matchups#teams_seasons', as: 'teams_seasons'
   get 'rankings', to: 'matchups#rankings', as: 'rankings'
 end
